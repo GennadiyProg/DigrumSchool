27c11fe7232bbdabeb5028d21f4dd9a7b5b998f4
GennadiyProg <grumancev9@gmail.com>
2022-11-26 13:31:07 +0300

    Небольшие улучшение

--- DigrumSchool/Models/User.cs
+++ DigrumSchool/Models/User.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace DigrumSchool.Models
 {
     public class User
     {
         public int Id { get; set; }
+        [JsonIgnore]
         public string Password { get; set; }
         public string Username { get; set; }
         public ICollection<Course> Courses { get; set; }
