3c81977f71ffa8c688cb2c8538a16a5b7ce9cca3
GennadiyProg <grumancev9@gmail.com>
2022-12-19 07:24:22 +0300

    Добавлены тесты для сервисов теста и пользователя

---
+++ DigrumSchool.Tests/Services/TestService.Test.cs
@@ -0,0 +1,139 @@
+﻿using DigrumSchool.Config;
+using DigrumSchool.Dto;
+using DigrumSchool.Models;
+using DigrumSchool.Models.Dto;
+using DigrumSchool.Services;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using Xunit;
+
+namespace DigrumSchool.Tests
+{
+    public class TestServiceTest
+    {
+        private TestService testService;
+        private SchoolContext context;
+        private User user;
+
+        public TestServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<SchoolContext>()
+                .UseInMemoryDatabase(databaseName: "DigrumSchool")
+                .Options;
+            context = new SchoolContext(options);
+            testService = new TestService(context);
+
+            user = new User()
+            {
+                Username = "user",
+                Password = "pass",
+                Role = new Role() { Name = "User" }
+            };
+            context.Users.Add(user);
+            context.SaveChanges();
+        }
+
+        [Fact]
+        public void CreatedTestIsSavedWhenCreateTest()
+        {
+            TestDto test = new TestDto()
+            {
+                Title = "Test",
+                Words = new List<WordDto>() { 
+                    new WordDto()
+                        {
+                            Name = "red",
+                            Translations = new List<string> { "красный" }
+                        } 
+                } ,
+                IsGeneral = false
+            };
+
+            testService.Create(test, user);
+
+            Assert.True(context.Tests.ToList().Any());
+        }
+
+        [Fact]
+        public void UserIsSetAsCreatorWhenCreateTest()
+        {
+            TestDto test = new TestDto()
+            {
+                Title = "Test",
+                Words = new List<WordDto>() {
+                    new WordDto()
+                        {
+                            Name = "red",
+                            Translations = new List<string> { "красный" }
+                        }
+                },
+                IsGeneral = false
+            };
+
+            Test createdTest = testService.Create(test, user);
+
+            Assert.NotNull(createdTest);
+            Assert.NotNull(createdTest.Creator);
+            Assert.Equal(createdTest.Creator.Username, user.Username);
+        }
+
+        [Fact]
+        public void TestBecomeIsGeneralWhenUserIsAdmin()
+        {
+            TestDto test = new TestDto()
+            {
+                Title = "Test",
+                Words = new List<WordDto>() {
+                    new WordDto()
+                        {
+                            Name = "red",
+                            Translations = new List<string> { "красный" }
+                        }
+                },
+                IsGeneral = true
+            };
+
+            User admin = new User()
+            {
+                Username = "user",
+                Password = "pass",
+                Role = new Role() { Name = "Admin" }
+            };
+
+            Test testWithCommonUser = testService.Create(test, user);
+            Test testWithAdmin = testService.Create(test, admin);
+
+            Assert.NotNull(testWithCommonUser);
+            Assert.NotNull(testWithAdmin);
+            Assert.False(testWithCommonUser.IsGeneral);
+            Assert.True(testWithAdmin.IsGeneral);
+        }
+
+        [Fact]
+        public void TestIsCompleteWhenCompleteTest()
+        {
+            TestDto test = new TestDto()
+            {
+                Title = "Test",
+                Words = new List<WordDto>() {
+                    new WordDto()
+                        {
+                            Name = "red",
+                            Translations = new List<string> { "красный" }
+                        }
+                },
+                IsGeneral = true
+            };
+            Test createdTest = testService.Create(test, user);
+            CompletedTest completedTest = testService.CompleteTest(new CompletedTestDto()
+            {
+                Score = 100,
+                TestId = createdTest.Id
+            }, user);
+
+            Assert.NotNull(completedTest);
+            Assert.NotNull(completedTest.Test);
+            Assert.Equal(completedTest.Test.Id, createdTest.Id);
+        }
+    }
+}
---
+++ DigrumSchool.Tests/Services/UserService.Test.cs
@@ -0,0 +1,69 @@
+﻿using DigrumSchool.Config;
+using DigrumSchool.Dto;
+using DigrumSchool.Models;
+using DigrumSchool.Services;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigrumSchool.Tests.Services
+{
+    public class UserServiceTest
+    {
+        private UserService userService;
+        private SchoolContext context;
+
+        public UserServiceTest()
+        {
+            var options = new DbContextOptionsBuilder<SchoolContext>()
+                .UseInMemoryDatabase(databaseName: "DigrumSchool")
+                .Options;
+            context = new SchoolContext(options);
+            userService = new UserService(context);
+        }
+
+        [Fact]
+        public void RegisterReturnNullWhenUsernameAlreadyExist()
+        {
+            context.Roles.Add(new Role { Name = "User" });
+            context.SaveChanges();
+            UserDto userDto = new UserDto()
+            {
+                UserName = "user",
+                Password = "pass"
+            };
+
+            User? firstUser = userService.Register(userDto);
+            User? secondUser = userService.Register(userDto);
+
+            Assert.NotNull(firstUser);
+            Assert.Null(secondUser);
+        }
+
+        [Fact]
+        public void LoginReturnNullWhenPasswordNotEquals()
+        {
+            context.Roles.Add(new Role { Name = "User" });
+            context.SaveChanges();
+            UserDto userDto = new UserDto()
+            {
+                UserName = "newUser",
+                Password = "pass"
+            };
+
+            User? registerUser = userService.Register(userDto);
+            Assert.NotNull(registerUser);
+
+            User? loginUser = userService.Login(new UserDto()
+            {
+                UserName = registerUser.Username,
+                Password = registerUser.Password + "1"
+            });
+
+            Assert.Null(loginUser);
+        }
+    }
+}
